214492b47bfef411d4b5ac016d83d59ee012b375
tsy1170 <tsy1170@gmail.com>
2025-06-25 02:22:03 +0800

    Update Question_2.py

--- Question_2.py
+++ Question_2.py
@@ -5,6 +5,8 @@ class UDGraph:
     def addVertex(self, vertex):
         if vertex not in self.adj_list:
             self.adj_list[vertex] = []
+        else:
+            print("Vertex already exists")
 
     def addEdge(self, from_vertex, to_vertex):
         if from_vertex in self.adj_list and to_vertex in self.adj_list:
@@ -15,29 +17,36 @@ class UDGraph:
     def listOutGoingAdjacentVertex(self, vertex):
         if vertex in self.adj_list:
             return self.adj_list.get(vertex, [])
+        else:
+            print("Vertex not founded")
 
     def print_adj_list(self):
         for vertex in self.adj_list:
             print(f"{vertex} -> {self.adj_list[vertex]}")
 
 
-g = UDGraph()
-vertex = ['A', 'B', 'C', 'D']
+class Person:
+    def __init__(self, name, age, gender, email):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.email = email
 
-for i in range(len(vertex)):
-    g.addVertex(vertex[i])
+    def get_person_detail(self):
+        return (f"Name  : {self.name}\n"
+                f"Age   : {self.age}\n"
+                f"Gender: {self.gender}\n"
+                f"E-mail: {self.email}")
 
-g.addEdge(vertex[0], vertex[1])
-g.addEdge(vertex[0], vertex[2])
-g.addEdge(vertex[0], vertex[3])
-g.addEdge(vertex[1], vertex[0])
-g.addEdge(vertex[1], vertex[2])
-g.addEdge(vertex[1], vertex[3])
-g.addEdge(vertex[2], vertex[1])
-g.addEdge(vertex[2], vertex[3])
-g.addEdge(vertex[3], vertex[2])
+    def __str__(self):
+        return f"{self.name}"
 
-g.print_adj_list()
-print()
-print(g.listOutGoingAdjacentVertex(vertex[0]))
+    def __repr__(self):
+        return self.__str__()
 
+
+alice = Person("Alice", "20", "Female", "alice20@gmail.com")
+ben = Person("Ben", "21", "Male", "ben21@gmail.com")
+chloe = Person("Chloe", "22", "Female", "chloe22@gmail.com")
+alex = Person("Alex", "20", "Male", "alex20@gmail.com")
+emma = Person("Emma", "19", "Female", "emma19@gmail.com")
