f40c90bc8f534c41aafd8bf47f85af194c4e63b9
tsy1170 <tsy1170@gmail.com>
2025-06-25 14:48:56 +0800

    Update and rename Question_2.py to Question_2.1.py

--- Question_2.1.py
+++ Question_2.1.py
@@ -23,30 +23,3 @@ class UDGraph:
     def print_adj_list(self):
         for vertex in self.adj_list:
             print(f"{vertex} -> {self.adj_list[vertex]}")
-
-
-class Person:
-    def __init__(self, name, age, gender, email):
-        self.name = name
-        self.age = age
-        self.gender = gender
-        self.email = email
-
-    def get_person_detail(self):
-        return (f"Name  : {self.name}\n"
-                f"Age   : {self.age}\n"
-                f"Gender: {self.gender}\n"
-                f"E-mail: {self.email}")
-
-    def __str__(self):
-        return f"{self.name}"
-
-    def __repr__(self):
-        return self.__str__()
-
-
-alice = Person("Alice", "20", "Female", "alice20@gmail.com")
-ben = Person("Ben", "21", "Male", "ben21@gmail.com")
-chloe = Person("Chloe", "22", "Female", "chloe22@gmail.com")
-alex = Person("Alex", "20", "Male", "alex20@gmail.com")
-emma = Person("Emma", "19", "Female", "emma19@gmail.com")
